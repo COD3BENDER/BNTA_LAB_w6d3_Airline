1c3a5eca37bcb4dca547cfb049b85aeb5410422d
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-08-09 16:59:19 +0100

    init commit

---
+++ src/main/java/com/bnta/labwk6d3airline/Labwk6d3AirlineApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.labwk6d3airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labwk6d3AirlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labwk6d3AirlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnta/labwk6d3airline/Labwk6d3AirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.labwk6d3airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labwk6d3AirlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
